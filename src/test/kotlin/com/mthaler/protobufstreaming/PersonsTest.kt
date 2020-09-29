1a7199b204724ff0202f876901737b6afb3e6767
Michael Thaler <michael.thaler@dlr.de>
2020-09-29 14:43:35 +0200

    Added PersonsTest.testDeserializeStreaming.

--- src/test/kotlin/com/mthaler/protobufstreaming/PersonsTest.kt
+++ src/test/kotlin/com/mthaler/protobufstreaming/PersonsTest.kt
@@ -1,7 +1,8 @@
 package com.mthaler.protobufstreaming
 
+import com.google.protobuf.CodedInputStream
 import com.google.protobuf.CodedOutputStream
-import com.google.protobuf.WireFormat
+import com.google.protobuf.ExtensionRegistryLite
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 import java.io.ByteArrayOutputStream
@@ -33,6 +34,37 @@ class PersonsTest {
         cout.flush()
         val data = out.toByteArray()
         val result = PersonsOuterClass.Persons.parseFrom(data)
+        assertEquals(persons, result)
+    }
+
+    @Test
+    fun testDeserializeStreaming() {
+        val out = ByteArrayOutputStream()
+        val p0 = PersonOuterClass.Person.newBuilder().setName("Richard Feynman").setId(42).setEmail("feynman@caltech.edu").build()
+        val p1 = PersonOuterClass.Person.newBuilder().setName("Hans Bethe").setId(42).setEmail("bethe@caltech.edu").build()
+        val persons = PersonsOuterClass.Persons.newBuilder().addPerson(p0).addPerson(p1).build()
+        persons.writeTo(out)
+        val data = out.toByteArray()
+        val cin = CodedInputStream.newInstance(data)
+        val registry = ExtensionRegistryLite.getEmptyRegistry()
+        val b = PersonsOuterClass.Persons.newBuilder()
+        var done = false
+        while (!done) {
+            val tag = cin.readTag()
+            when(tag) {
+                0 -> {
+                    done = true
+                }
+                10 -> {
+                    val p = cin.readMessage(PersonOuterClass.Person.PARSER, registry)
+                    b.addPerson(p)
+                }
+                else -> {
+                    done = true
+                }
+            }
+        }
+        val result = b.build()
         assertEquals(persons, result)
     }
 }
